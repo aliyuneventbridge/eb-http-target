dfe9742211a674595f4fd5bdd7b7eb027379a455
zhouxinyu <zhou@xinyu.im>
2020-10-30 15:45:04 +0800

    Support body trace

--- src/main/java/com/aliyuneventbridge/httptarget/HttpTargetApplication.java
+++ src/main/java/com/aliyuneventbridge/httptarget/HttpTargetApplication.java
@@ -1,7 +1,16 @@
 package com.aliyuneventbridge.httptarget;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +18,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpTargetApplication {
 
+    List<Map<String, Map<String, String>>> requestLists = new CopyOnWriteArrayList<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(HttpTargetApplication.class, args);
 	}
 
     @RequestMapping("/cloudevents")
-    public String onEvents() {
-        return "OK";
+    public String onEvents(@RequestHeader Map<String, String> headers, @RequestBody Map<String, String> event) {
+        Map<String, Map<String, String>> request = new HashMap<>();
+        request.put("HttpHeaders", headers);
+        request.put("HttpBody", event);
+        requestLists.add(0, request);
+
+        if (requestLists.size() > 10) {
+            requestLists.remove(requestLists.size() - 1);
+        }
+
+	    return "OK";
+    }
+
+    @RequestMapping("/httptrace")
+    public String requestLists() {
+        Gson gson = new GsonBuilder()
+            .setPrettyPrinting()
+            .create();
+        return gson.toJson(requestLists);
     }
 }
 
