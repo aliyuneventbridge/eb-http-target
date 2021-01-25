677843c9854e09e7c6e0383e972f932450566697
jingluo.sl <jingluo.sl@alibaba-inc.com>
2021-01-25 19:49:44 +0800

    feat:add public key for same lanucher can not connect to public network

--- src/main/java/com/aliyuneventbridge/httptarget/siginature/PublicKeyBuilder.java
+++ src/main/java/com/aliyuneventbridge/httptarget/siginature/PublicKeyBuilder.java
@@ -37,43 +37,42 @@ public class PublicKeyBuilder {
     }
 
     private static String downloadCert(URI certUrl) {
-        try {
-            verifyCertUrl(certUrl);
-            HttpResponse response = client.execute(new HttpGet(certUrl));
-            if (response.getStatusLine()
-                .getStatusCode() / 100 == 2) {
-                try {
+        //try {
+            //verifyCertUrl(certUrl);
+            //HttpResponse response = client.execute(new HttpGet(certUrl));
+            //if (response.getStatusLine()
+            //    .getStatusCode() / 100 == 2) {
+            //    try {
                     String publicCerl = "-----BEGIN CERTIFICATE-----\n"
-                        + "MIICwzCCAiwCCQC++87W+yaNEDANBgkqhkiG9w0BAQUFADCBpTELMAkGA1UEBhMC\n"
+                        + "MIICxTCCAi4CCQCTtXSvs6sFWTANBgkqhkiG9w0BAQUFADCBpTELMAkGA1UEBhMC\n"
                         + "Q04xETAPBgNVBAgMCFpoZUppYW5nMREwDwYDVQQHDAhIYW5nWmhvdTEPMA0GA1UE\n"
                         + "CgwGQWxpeXVuMRMwEQYDVQQLDApNaWRkbGV3YXJlMR8wHQYDVQQDDBZldmVudGJy\n"
                         + "aWRnZS5hbGl5dW4uY29tMSkwJwYJKoZIhvcNAQkBFhpqaW5nbHVvLnNsQGFsaWJh\n"
-                        + "YmEtaW5jLmNvbTAeFw0yMTAxMjExMzIwMzdaFw0zMTAxMTkxMzIwMzdaMIGlMQsw\n"
-                        + "CQYDVQQGEwJDTjERMA8GA1UECAwIWmhlSmlhbmcxETAPBgNVBAcMCEhhbmdaaG91\n"
-                        + "MQ8wDQYDVQQKDAZBbGl5dW4xEzARBgNVBAsMCk1pZGRsZXdhcmUxHzAdBgNVBAMM\n"
-                        + "FmV2ZW50YnJpZGdlLmFsaXl1bi5jb20xKTAnBgkqhkiG9w0BCQEWGmppbmdsdW8u\n"
-                        + "c2xAYWxpYmFiYS1pbmMuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDG\n"
-                        + "HZD42MuTIMsyAilDiLCq73emBwQ5eYWzY+lDG9JCwZPnI73hgq/JG1r+7gurn7mH\n"
-                        + "oSgzmavRcJQzu8mAZHPBaneohetL5i4lmsPWV/Ef869OmBZi+j0rjUgJmTjAV9Vr\n"
-                        + "qTppK/rO8gedBSIiymEWpR1rgzqYZvrMrFrcFy0oqwIDAQABMA0GCSqGSIb3DQEB\n"
-                        + "BQUAA4GBAE8FQjH6SDM2CMU4RBWuI2/jqpDJtg7qc30aasVRyjlbxhSGBkzmAYnM\n"
-                        + "xwyJSfQImDs/n35uuYE3pKH0bLRlOQu9Z0TlumSRlhin+YXBfPNScoyX8upNfLgr\n"
-                        + "peHK7YGGpAbJH8Qh3GbOueSD1wT/MWA15QfQABHgtrYl5Am+VNNc\n" + "-----END CERTIFICATE-----";
+                        + "YmEtaW5jLmNvbTAgFw0yMTAxMjUwOTIzMjZaGA8yMTIxMDEwMTA5MjMyNlowgaUx\n"
+                        + "CzAJBgNVBAYTAkNOMREwDwYDVQQIDAhaaGVKaWFuZzERMA8GA1UEBwwISGFuZ1po\n"
+                        + "b3UxDzANBgNVBAoMBkFsaXl1bjETMBEGA1UECwwKTWlkZGxld2FyZTEfMB0GA1UE\n"
+                        + "AwwWZXZlbnRicmlkZ2UuYWxpeXVuLmNvbTEpMCcGCSqGSIb3DQEJARYaamluZ2x1\n"
+                        + "by5zbEBhbGliYWJhLWluYy5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGB\n"
+                        + "AMtqUJ8GfVzgCiyUzWjpe3Y+7Ub/GERfqS1zAneVUEXJvRssovrqy4eGXwPBpACG\n"
+                        + "MDf7iOK7sCk1lKDFDGjNk34SsR6zcpq8h3z2fHIzEwqSgo4tdRjAEeSBUTrGlVU/\n"
+                        + "suyMlcPiXDAYGOiWc2CISpD/709Ni7xHZttfeh3sde1/AgMBAAEwDQYJKoZIhvcN\n"
+                        + "AQEFBQADgYEADWYxzJAAtteTEcJVf5HUgIDbqwJwTS198HxLRhUSdwy9jBh7VpZo\n"
+                        + "3UOsqW+oU9TdFkaHsmVQWbH6UzoOtCeZiU45z8G6nxFssg0ZR94U4pXI4od4GaH3\n"
+                        + "485p83bOGV5jRmsS0winbtDQBrkTrsRB0hfPogoCwVo/p3MAYbCmnJY=\n" + "-----END CERTIFICATE-----";
                         //new String(toByteArray(response.getEntity()
                         //.getContent()), Charset.forName("UTF-8"));
-                    System.out.println("publicCerl:" + publicCerl);
                     return publicCerl;
-                } finally {
-                    response.getEntity()
-                        .getContent()
-                        .close();
-                }
-            } else {
-                throw new RuntimeException("Could not download the certificate from EventBridge:" + certUrl);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException("Unable to download SNS certificate from " + certUrl.toString(), e);
-        }
+                //} finally {
+                //    response.getEntity()
+                //        .getContent()
+                //        .close();
+                //}
+            //} else {
+            //    throw new RuntimeException("Could not download the certificate from EventBridge:" + certUrl);
+            //}
+        //} catch (IOException e) {
+        //    throw new RuntimeException("Unable to download SNS certificate from " + certUrl.toString(), e);
+        //}
     }
 
     public static byte[] toByteArray(InputStream is) throws IOException {
